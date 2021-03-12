aac058980c1d903296daa34a3b31f0bcde392cf2
bruno talarico <bruno.talarico@gmail.com>
2021-03-12 13:50:42 +0000

    migration jsx -> tsx
    Signed-off-by: bruno talarico <bruno.talarico@gmail.com>

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -5,14 +5,15 @@ import "../styles/repositories.scss";
 
 //https://api.github.com/orgs/rocketseat/repos
 
-const repository = {
-  name: "unform",
-  description: "Forms in React",
-  link: "https://github.com/unform/unform",
-};
+interface Repository {
+  id: number;
+  description: string;
+  html_url: string;
+  name: string;
+}
 
 export default function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(() => {
     fetch("https://api.github.com/orgs/rocketseat/repos")
